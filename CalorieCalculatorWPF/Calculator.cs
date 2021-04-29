f65c66272f7843a28a8a3823c8a366d4176766bf
Drake Detar <Drakedetar33@ou.edu>
2021-04-28 20:23:23 -0500

    Fixed up XAML code

---
+++ CalorieCalculatorWPF/Calculator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calorie_Calculator
+{
+    class Calculator
+    {
+        // Height/ Weight/ age/ bmr/ cal. intake/ gender
+        public double Height { get; set; }
+        public double Weight { get; set; }
+        public int Age { get; set; }
+        public double ActivityLevel { get; set; }
+        public string Gender { get; set; }
+
+        public double BMR = 0;
+
+        public double CalorieIntake=0;
+        public Calculator()
+        {
+            Height = 0;
+            Weight = 0;
+            Age = 0;
+            ActivityLevel = 0;
+            Gender = "";
+        }
+
+        public Calculator(double height, double weight, int age, int activityLevel, string gender)
+        {
+            Height = height;
+            Weight = weight;
+            Age = age;
+            ActivityLevel = activityLevel;
+            Gender = gender;
+
+            switch (ActivityLevel)
+            {
+                case 1:
+                    ActivityLevel = 1.2;
+                    break;
+                case 2:
+                    ActivityLevel = 1.375;
+                    break;
+                case 3:
+                    ActivityLevel = 1.65;
+                    break;
+                default:
+                    ActivityLevel = 1.9;
+                    break;
+            }
+
+            if (Gender.ToLower() == "male")
+            {
+                BMR = 66 + (6.3 * Weight) + (12.9 * Height) - (6.8 * Age);
+                CalorieIntake = BMR * ActivityLevel;
+                Console.WriteLine($"Your daily caloric intake is {CalorieIntake} calories");
+            }
+            else if (Gender.ToLower() == "female")
+            {
+                BMR = 655 + (4.3 * weight) + (4.7 * height) - (4.7 * age);
+                CalorieIntake = BMR * ActivityLevel;
+                Console.WriteLine($"Your daily caloric intake is {CalorieIntake} calories");
+            }
+        }
+
+    }
+}
---
+++ CalorieCalculatorWPF/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace CalorieCalculatorWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void S_Button(object sender, RoutedEventArgs e)
+        {
+            int height = Convert.ToInt32(HeightInput.Text);
+            int weight = Convert.ToInt32(WeightInput.Text);
+            int age = Convert.ToInt32(AgeInput.Text);
+            double activityLevel = 0;
+            int genderBMR = 0;
+            double BMR = 0;
+            double caloric_intake = 0;
+
+            if (Convert.ToBoolean(MaleRB.IsChecked))
+            {
+                genderBMR = 5;
+            }
+            else
+            {
+                genderBMR = -161;
+            }
+            switch (activityLevel)
+            {
+                default:
+                    break;
+            }
+            BMR = (4.536 * weight) + (15.88 * height) - (5 * age) + genderBMR;
+
+            if (Convert.ToBoolean(Intake1.IsChecked))
+            {
+                activityLevel = 1.2;
+            }
+            else if (Convert.ToBoolean(Intake2.IsChecked))
+            {
+                activityLevel = 1.375;
+            }
+            else if (Convert.ToBoolean(Intake3.IsChecked))
+            {
+                activityLevel = 1.65;
+            }
+            else
+            {
+                activityLevel = 1.9;
+            }
+
+            caloric_intake = BMR * activityLevel;
+            OutputBox.Text = $"Your daily caloric intake is {Convert.ToString(caloric_intake)}";
+            
+
+        }
+    }
+}
